d57792c656888f2d557fbb1bde78ee4aa14cf054
Md Atif Ansar <atifansar002@gmail.com>
2021-05-21 10:46:13 +0530

    carousel modify

--- src/collectionPage.js
+++ src/collectionPage.js
@@ -140,45 +140,7 @@ return (
     </div>
     </Carousel.Item>)
     )}
-
-  
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://source.unsplash.com/random/flower"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-
-
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://source.unsplash.com/random/cartoon"
-      alt="Second slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://source.unsplash.com/random/anime"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
+    
 </Carousel>
 
 
